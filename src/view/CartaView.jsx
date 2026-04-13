291c8cc35916ae5f2172aed01698efd623a4e11f
Alejandro Caloto <alejandrocaloto1@gmail.com>
2026-04-13 19:10:52 +0200

    feat: implementación de la estructura inicial de la pagina web y movimiento entre ventanas

---
+++ src/view/CartaView.jsx
@@ -0,0 +1,10 @@
+export default function CartaView() {
+  return (
+    <div className="view-container">
+      <h2 className="section-title">Nuestra Carta</h2>
+      <div className="placeholder-content">
+        <p>Contenido de la carta (Platos, Precios y Alérgenos).</p>
+      </div>
+    </div>
+  );
+}
